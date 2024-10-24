9836f7b340b159a7947adaf5a3757a305f1a77ed
Steven Carter <steve@javapl.us>
2024-10-24 00:59:44 -0400

    fix: Fix build from vite plugin eslint error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+// @ts-expect-error See https://github.com/gxmari007/vite-plugin-eslint/issues/79
 import eslint from 'vite-plugin-eslint';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
-const rawPlugin = function rawPlugin(options) {
+const rawPlugin = function rawPlugin(options: any) {
   return {
     name: 'vite-raw-plugin',
-    transform(code, id) {
+    transform(code: any, id: any) {
       if (options.fileRegex.test(id)) {
         const json = JSON.stringify(code)
           .replace(/\u2028/g, '\\u2028')
