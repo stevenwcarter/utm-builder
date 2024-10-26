c9f59e93dc802188b5c84259f8ee6b0e247472d9
Steven Carter <steve@javapl.us>
2024-10-26 12:57:15 -0400

    test: Adding junit reporting for beta codecov feature

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig((configEnv) =>
         environment: 'jsdom',
         // setupFiles: ['./setupVitest.ts'],
         coverage: {
-          reporter: ['text', 'html', 'cobertura', 'lcov', 'json-summary'],
+          reporter: ['junit', 'text', 'html', 'cobertura', 'lcov', 'json-summary'],
           exclude: [
             '**/[.]**',
             '**/*.cjs',
