4a6495045ae80ab89ce0b33de19bcedb86c86b04
Steven Carter <steve@javapl.us>
2024-10-26 11:58:50 -0400

    test: Added line numbers for each test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -26,54 +26,91 @@ describe('urlBuilder', () => {
     );
   });
 
-  it('uses examples', () => {
-    const tests = rawTests.split('\n');
+  it('adds campaign name', () => {
+    const props: UrlBuilderProps = {
+      url: 'http://foo.com',
+      campaignName: 'bar',
+    };
 
-    let count = 0;
+    expect(urlBuilder(props)).toBe('http://foo.com?utm_campaign=bar');
+  });
 
-    tests.forEach((test) => {
-      count++;
-      const [
-        url,
-        source,
-        medium,
-        campaignName,
-        branding,
-        targetingTactic,
-        state,
-        city,
-        businessUnit,
-        creativeType,
-        creativeVariant,
-        audience,
-        content,
-        term,
-        resultUrl,
-      ] = test.split('\t');
+  it('adds utm_content if campaignName does not have pmax', () => {
+    const props: UrlBuilderProps = {
+      url: 'http://foo.com',
+      campaignName: 'bar',
+      content: 'foobar',
+    };
 
-      const result = urlBuilder({
-        url,
-        source,
-        medium,
-        campaignName,
-        branding,
-        targetingTactic,
-        state,
-        city,
-        businessUnit,
-        creativeType,
-        creativeVariant,
-        audience,
-        content,
-        term,
-      });
+    expect(urlBuilder(props)).toBe('http://foo.com?utm_campaign=bar&utm_content=foobar');
+  });
 
-      console.log('On item: ', count);
-      const urlEncoded = unBraces(url?.trim());
+  it('adds utm_term if campaignName does not have pmax', () => {
+    const props: UrlBuilderProps = {
+      url: 'http://foo.com',
+      campaignName: 'bar',
+      term: 'term_bar',
+    };
 
-      if (resultUrl !== '') {
-        expect(result).toBe((unBraces(resultUrl) || '').replace(urlEncoded || '', url?.trim()));
-      }
+    expect(urlBuilder(props)).toBe('http://foo.com?utm_campaign=bar&utm_term=term_bar');
+  });
+
+  it('adds both utm_content and utm_term if campaignName does not have pmax', () => {
+    const props: UrlBuilderProps = {
+      url: 'http://foo.com',
+      campaignName: 'bar',
+      content: 'foo_content',
+      term: 'term_bar',
+    };
+
+    expect(urlBuilder(props)).toBe(
+      'http://foo.com?utm_campaign=bar&utm_content=foo_content&utm_term=term_bar',
+    );
+  });
+
+  const tests = rawTests.split('\n');
+  const example_tests = tests.map((t, i) => [i, t, t.split('\t').slice(-1)[0]]);
+
+  it.each(example_tests)('example: %d', (_lineNum, test, _endUrl) => {
+    const [
+      url,
+      source,
+      medium,
+      campaignName,
+      branding,
+      targetingTactic,
+      state,
+      city,
+      businessUnit,
+      creativeType,
+      creativeVariant,
+      audience,
+      content,
+      term,
+      resultUrl,
+    ] = (test as string).split('\t');
+
+    const result = urlBuilder({
+      url,
+      source,
+      medium,
+      campaignName,
+      branding,
+      targetingTactic,
+      state,
+      city,
+      businessUnit,
+      creativeType,
+      creativeVariant,
+      audience,
+      content,
+      term,
     });
+
+    const urlEncoded = unBraces(url?.trim());
+
+    if (resultUrl !== '') {
+      expect(result).toBe((unBraces(resultUrl) || '').replace(urlEncoded || '', url?.trim()));
+    }
   });
 });
--- src/urlBuilder.ts
+++ src/urlBuilder.ts
@@ -16,13 +16,6 @@ export interface UrlBuilderProps {
 }
 export type UrlBuilderPropsWithResult = UrlBuilderProps & { urlResult: string };
 
-// http://www.constellation.com
-// ?utm_source=google
-// utm_medium=cpc
-// utm_campaign=test1_br_pr_tx_atl_en
-// utm_content=%7Bad_content%7D
-// utm_term=%7Bkeyword%7D
-
 export const lp = (s?: string) => s?.toLowerCase().trim();
 export const unBraces = (s?: string) =>
   s
@@ -34,132 +27,114 @@ export const unBraces = (s?: string) =>
     .replace(/:/g, '%3A')
     .replace(/%26utm/g, '&utm');
 
-export const urlBuilder = (d: UrlBuilderProps) => {
-  const params = new URLSearchParams({});
-  const source = lp(d.source);
+const googleCpc = (d: UrlBuilderProps, params: URLSearchParams) => {
+  const campaign = [
+    lp(d.campaignName),
+    lp(d.branding),
+    lp(d.targetingTactic),
+    lp(d.state),
+    lp(d.city),
+    lp(d.businessUnit),
+  ]
+    .filter((a) => a !== '')
+    .join('_');
 
-  if (source && source !== '') {
-    params.append('utm_source', source);
+  if (campaign && campaign !== '') {
+    params.append('utm_campaign', campaign);
   }
 
-  const medium = lp(d.medium);
-
-  if (medium && medium !== '') {
-    params.append('utm_medium', medium);
+  if (lp(d.campaignName)?.indexOf('pmax') === -1) {
+    params.append('utm_content', '{ad_content}');
+    params.append('utm_term', '{keyword}');
   }
 
-  if (source === 'google' && medium === 'cpc') {
-    const campaign = [
-      lp(d.campaignName),
-      lp(d.branding),
-      lp(d.targetingTactic),
-      lp(d.state),
-      lp(d.city),
-      lp(d.businessUnit),
-    ]
-      .filter((a) => a !== '')
-      .join('_');
+  return `${d.url?.trim()}?${params.toString()}`;
+};
 
-    if (campaign && campaign !== '') {
-      params.append('utm_campaign', campaign);
-    }
+const mediaHandler = (d: UrlBuilderProps, params: URLSearchParams) => {
+  const campaign = [lp(d.campaignName), lp(d.targetingTactic), lp(d.businessUnit)]
+    .filter((a) => a !== '')
+    .join('_');
 
-    if (lp(d.campaignName)?.indexOf('pmax') === -1) {
-      params.append('utm_content', '{ad_content}');
-      params.append('utm_term', '{keyword}');
-    }
-
-    return params.toString() !== '' ? `${d.url?.trim()}?${params.toString()}` : '';
+  if (campaign && campaign !== '') {
+    params.append('utm_campaign', campaign);
   }
 
-  if (source === 'bing' && medium === 'cpc') {
-    const campaign = [
-      lp(d.campaignName),
-      lp(d.branding),
-      lp(d.targetingTactic),
-      lp(d.state),
-      lp(d.city),
-      lp(d.businessUnit),
-    ]
-      .filter((a) => a !== '')
-      .join('_');
+  if (lp(d.campaignName)?.indexOf('pmax') === -1) {
+    params.append(
+      'utm_content',
+      [lp(d.creativeVariant), lp(d.state), lp(d.city)].filter((a) => a !== '').join('_'),
+    );
+    const audience = lp(d.audience);
 
-    if (campaign && campaign !== '') {
-      params.append('utm_campaign', campaign);
+    if (audience) {
+      params.append('utm_term', audience);
     }
-
-    if (lp(d.campaignName)?.indexOf('pmax') === -1) {
-      params.append('utm_content', '{AdId}');
-      params.append('utm_term', '{keyword:keyword}');
-    }
-
-    return params.toString() !== '' ? `${d.url?.trim()}?${params.toString()}` : '';
   }
 
-  if (
-    medium === 'display' ||
-    medium === 'native_display' ||
-    medium === 'video' ||
-    medium === 'audio'
-  ) {
-    const campaign = [lp(d.campaignName), lp(d.targetingTactic), lp(d.businessUnit)]
-      .filter((a) => a !== '')
-      .join('_');
+  return `${d.url?.trim()}?${params.toString()}`;
+};
 
-    if (campaign && campaign !== '') {
-      params.append('utm_campaign', campaign);
-    }
+const bingCpc = (d: UrlBuilderProps, params: URLSearchParams) => {
+  const campaign = [
+    lp(d.campaignName),
+    lp(d.branding),
+    lp(d.targetingTactic),
+    lp(d.state),
+    lp(d.city),
+    lp(d.businessUnit),
+  ]
+    .filter((a) => a !== '')
+    .join('_');
 
-    if (lp(d.campaignName)?.indexOf('pmax') === -1) {
-      params.append(
-        'utm_content',
-        [lp(d.creativeVariant), lp(d.state), lp(d.city)].filter((a) => a !== '').join('_'),
-      );
-      const audience = lp(d.audience);
-
-      if (audience) {
-        params.append('utm_term', audience);
-      }
-    }
-
-    return params.toString() !== '' ? `${d.url?.trim()}?${params.toString()}` : '';
+  if (campaign && campaign !== '') {
+    params.append('utm_campaign', campaign);
   }
 
-  if (medium?.replace(/ /g, '') === 'paidsocial') {
-    const campaign = [lp(d.campaignName), lp(d.targetingTactic), lp(d.businessUnit)]
-      .filter((a) => a !== '')
-      .join('_');
-
-    if (campaign && campaign !== '') {
-      params.append('utm_campaign', campaign);
-    }
-
-    if (lp(d.campaignName)?.indexOf('pmax') === -1) {
-      params.append(
-        'utm_content',
-        [lp(d.creativeType), lp(d.creativeVariant), lp(d.state), lp(d.city)]
-          .filter((a) => a !== '')
-          .join('_'),
-      );
-      params.append(
-        'utm_term',
-        ['{{placement}}', lp(d.audience)]
-          .filter((a) => a !== '')
-          .join('_')
-          .replace('__', '_'),
-      );
-    }
-
-    return params.toString() !== '' ? `${d.url?.trim()}?${params.toString()}` : '';
+  if (lp(d.campaignName)?.indexOf('pmax') === -1) {
+    params.append('utm_content', '{AdId}');
+    params.append('utm_term', '{keyword:keyword}');
   }
 
+  return `${d.url?.trim()}?${params.toString()}`;
+};
+
+const paidSocialHandler = (d: UrlBuilderProps, params: URLSearchParams) => {
+  const campaign = [lp(d.campaignName), lp(d.targetingTactic), lp(d.businessUnit)]
+    .filter((a) => a !== '')
+    .join('_');
+
+  if (campaign && campaign !== '') {
+    params.append('utm_campaign', campaign);
+  }
+
+  if (lp(d.campaignName)?.indexOf('pmax') === -1) {
+    params.append(
+      'utm_content',
+      [lp(d.creativeType), lp(d.creativeVariant), lp(d.state), lp(d.city)]
+        .filter((a) => a !== '')
+        .join('_'),
+    );
+    params.append(
+      'utm_term',
+      ['{{placement}}', lp(d.audience)]
+        .filter((a) => a !== '')
+        .join('_')
+        .replace('__', '_'),
+    );
+  }
+
+  return `${d.url?.trim()}?${params.toString()}`;
+};
+
+const genericHandler = (d: UrlBuilderProps, params: URLSearchParams) => {
   const campaignName = lp(d.campaignName);
 
   if (campaignName && campaignName !== '') {
     params.append('utm_campaign', campaignName);
   }
 
-  if (lp(d.campaignName)?.indexOf('pmax') === -1) {
+  if (campaignName?.indexOf('pmax') === -1) {
     const content = lp(d.content);
 
     if (content) {
@@ -176,40 +151,35 @@ export const urlBuilder = (d: UrlBuilderProps) => {
   return params.toString() !== '' ? `${d.url?.trim()}?${params.toString()}` : '';
 };
 
-// = IFS(
-//   AND(LP(C201, "source")="google", LP(D201, "medium")="cpc"),
-//     TEXTJOIN("&", TRUE,
-//       URL(LPX(B201, "url"), LP(C201, "source"), LP(D201, "medium")),
-//       CONCATENATE("utm_campaign=", TEXTJOIN("_", TRUE, LP(E201,"campaign name"), LP(F201, "branding"), LP(G201, "targeting tactic"), LP(H201, "state"), LPO(I201, "city"), LP(J201, " business unit"))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_content=", "{ad_content}")),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_term=", "{keyword}"))
-//     ),
-//   AND(LP(C201, "source")="bing", LP(D201, "medium")="cpc"),
-//     TEXTJOIN("&", TRUE,
-//       URL(LPX(B201, "url"), LP(C201, "source"), LP(D201, "medium")),
-//       CONCATENATE("utm_campaign=", TEXTJOIN("_", TRUE, LP(E201,"campaign name"), LP(F201, "branding"), LP(G201, "targeting tactic"), LP(H201, "state"), LPO(I201, "city"), LP(J201, " business unit"))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_content=", "{AdId}")),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_term=", "{keyword:keyword}"))
-//     ),
-//   OR(LP(D201, "medium")="display", LP(D201, "medium")="native_display", LP(D201, "medium")="video", LP(D201, "medium")="audio"),
-//     TEXTJOIN("&", TRUE,
-//       URL(LPX(B201, "url"), LP(C201, "source"), LP(D201, "medium")),
-//       CONCATENATE("utm_campaign=", TEXTJOIN("_", TRUE, LP(E201,"campaign name"), LP(G201, "targeting tactic"), LP(J201, " business unit"))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_content=", TEXTJOIN("_", TRUE, LPO(L201,"creative variant"), LPO(H201, "state"), LPO(I201, "city")))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_term=", TEXTJOIN("_", TRUE, LPO(M201,"audience")))),
-//     ),
-//   OR(LP(D201, "medium")="paid+social"),
-//     TEXTJOIN("&", TRUE,
-//       URL(LPX(B201, "url"), LP(C201, "source"), LP(D201, "medium")),
-//       CONCATENATE("utm_campaign=", TEXTJOIN("_", TRUE, LP(E201,"campaign name"), LP(G201, "targeting tactic"), LP(J201, " business unit"))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_content=", TEXTJOIN("_", TRUE, LPO(K201,"creative type"), LPO(L201,"creative variant"), LPO(H201, "state"), LPO(I201, "city")))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_term=", TEXTJOIN("_", TRUE, "{{placement}}", LPO(M201,"audience")))),
-//     ),
-//   TRUE,
-//    TEXTJOIN("&", TRUE,
-//       URL(LPX(B201, "url"), LP(C201, "source"), LP(D201, "medium")),
-//       CONCATENATE("utm_campaign=", TEXTJOIN("_", TRUE, LP(E201,"campaign name"))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_content=", TEXTJOIN("_", TRUE, LPO(N201,"content")))),
-//       IF(ISNUMBER(SEARCH("pmax", E201)), "", CONCAT_IF("utm_term=", TEXTJOIN("_", TRUE, LPO(O201,"term")))),
-//     )
-// )
+export const urlBuilder = (d: UrlBuilderProps) => {
+  const params = new URLSearchParams({});
+  const source = lp(d.source);
+
+  if (source && source !== '') {
+    params.append('utm_source', source);
+  }
+
+  const medium = lp(d.medium);
+
+  if (medium && medium !== '') {
+    params.append('utm_medium', medium);
+  }
+
+  if (source?.toLowerCase() === 'google' && medium?.toLowerCase() === 'cpc') {
+    return googleCpc(d, params);
+  }
+
+  if (source?.toLowerCase() === 'bing' && medium?.toLowerCase() === 'cpc') {
+    return bingCpc(d, params);
+  }
+
+  if (medium && ['display', 'native_display', 'video', 'audio'].includes(medium.toLowerCase())) {
+    return mediaHandler(d, params);
+  }
+
+  if (medium?.replace(/ /g, '').toLowerCase() === 'paidsocial') {
+    return paidSocialHandler(d, params);
+  }
+
+  return genericHandler(d, params);
+};
